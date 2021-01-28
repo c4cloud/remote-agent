38196e6bd12bf3a09323a16f41b2f55edecf11ca
c4cloud <c4cloud>
2021-01-27 20:12:01 -0500

    server.js added

---
+++ node/server.js
@@ -0,0 +1,23 @@
+const http = require('http');
+const {spawn} = require('child_process');
+
+const hostname = '127.0.0.1';
+const port = 3001;
+const cmd = 'pwd';
+
+const server = http.createServer((req, res) => {
+
+     const child = spawn(cmd);
+     child.on('exit', function(code, signal){
+         console.log('child process exited with ' + `code: ${code} & signal: ${signal}`);
+     });
+
+
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Remote-Agent v0.0.1 is running...');
+});
+
+server.listen(port, hostname, () => {
+    console.log(`Remote-Agent Server is running at http://${hostname}:${port}/`);
+});
